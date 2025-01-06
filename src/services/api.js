c9b3795f4c7e0df01b85c936bfe9d93a7faa873e
Emad <emadkhanqai@gmail.com>
2025-01-06 15:02:19 +0500

    corrected api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL = 'https://api-dev-hayya-v2.hayya.qa';
 
 export const fetchForm = async (language = 'ar') => {
   const response = await axios.get(`${API_BASE_URL}/api/v1/Form`, {
-    params: { formType: 'uprofile', flowType: 'profile' },
+    params: { formType: 'uprofile', flowType: 'uprofile' },
     headers: { 'X-UserLanguage': language },
   });
   return response.data;
